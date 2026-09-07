f56d4444bcb16268b93ac6dcd6f0740809cbbdef
Wander Hayashida <wrhbra2013@gmail.com>
2026-09-06 21:11:41 -0300

    v0,1

--- static/js/form_admin.js
+++
@@ -1,9 +0,0 @@
-const senhaInput = document.getElementById('senha');
-const confirmarInput = document.getElementById('confirmar');
-const mostrarSenhaCheckbox = document.getElementById('mostrarSenha');
-
-mostrarSenhaCheckbox.addEventListener('change', function() {
-    const senhaType = senhaInput.type === 'password' ? 'text' : 'password';
-    senhaInput.type = senhaType;
-    confirmarInput.type = senhaType;
-});
--- static/js/form_castracao.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const clinicaSelect = document.getElementById('clinica');
-
-    clinicaSelect.addEventListener('change', function() {
-        if (this.value === '__nova_clinica__') {
-            window.location.href = '/clinicas/form';
-        }
-    });
-});
